5a49f065eb60b890dde84eb094764f59a4e074e8
LokeshShelva <58987457+LokeshShelva@users.noreply.github.com>
2020-05-02 00:29:42 +0530

    This is the python file

---
+++ create-repo.py
@@ -0,0 +1,57 @@
+import os
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+for x in sys.argv:
+    arg = x
+
+# WEB SCRAPING
+
+driver = webdriver.Chrome(executable_path='C:\Program Files (x86)\chromedriver')
+
+driver.get('http://www.github.com/login')
+
+try:
+    username = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "login_field"))
+    )
+    username.send_keys('###Enter your github email here###')
+
+    password = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "password"))
+    )
+    password.send_keys('###Enter your github password here###')
+    password.send_keys(Keys.RETURN)
+
+    driver.get('https://github.com/new')
+
+    repo_name = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "repository_name"))
+    )
+    repo_name.send_keys(str(arg))
+
+    time.sleep(3)
+
+    readMe = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, "repository_auto_init"))
+    )
+    readMe.click()
+
+    repo_name.send_keys(Keys.RETURN)
+    time.sleep(5)
+    url = driver.current_url
+# END OF WEB SCRAPING
+
+    repo = 'git clone ' + str(url) +'.git'
+    print(repo)
+    os.popen(repo)
+    os.popen('code .')
+
+except:
+    driver.quit()
